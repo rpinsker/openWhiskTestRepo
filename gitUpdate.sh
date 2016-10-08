1168a610844f3e0fde3bb14a7986c7473ec9f299
Rachel Pinsker <rachel.pinsker@gmail.com>
2016-10-07 19:54:56 -0500

    Commit #1.

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-echo "hello" > test.txt
-git add .
-git commit -m "Updating a file."
-git push
+
+for i in {1..4}
+do
+    if (($i%2=0));
+    then
+        echo "here"
+        str="hello"
+    else
+        echo "here!"
+        str="hi"
+    fi
+        echo $str > test.txt
+        git add .
+        git commit -m "Commit #${i}."
+        git push
+    done
+done
